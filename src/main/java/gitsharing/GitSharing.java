c47785aa9878828ac8fec6727eebc6388164cfdd
C.BALAJI <C.BALAJI@DESKTOP-U7B3DHD>
2018-05-19 22:29:06 +0530

    GitSharingVIgnesh

---
+++ src/main/java/gitsharing/GitSharing.java
@@ -0,0 +1,33 @@
+package gitsharing;
+
+public class GitSharing {
+
+	
+		public static void main(String[] args) {
+			// TODO Auto-generated method stub
+			//Program to print next sequence no 
+			
+			String text1="x1y2a3";
+			String str="";
+			
+			char[] charArray = text1.toCharArray();
+			
+			for(int i=0;i<=charArray.length-1;i++)
+			{
+				int ascii= charArray[i];
+				ascii = ascii+1;
+				char c = (char) ascii;
+				
+//				charArray=charArray+ascii;
+				//System.out.println(c);
+				System.out.print(str+c);
+				System.out.print("simple");
+				
+		
+			}
+			
+		}
+		
+		
+
+	}
